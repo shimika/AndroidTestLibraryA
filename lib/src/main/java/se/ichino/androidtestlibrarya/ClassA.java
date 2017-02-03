c4b11214fc1909abc8b08034b90261d29aeb2e78
Mika Jihoon Yoon <mika@zoyi.co>
2017-02-03 12:07:58 +0900

    Init

---
+++ lib/src/main/java/se/ichino/androidtestlibrarya/ClassA.java
@@ -0,0 +1,12 @@
+package se.ichino.androidtestlibrarya;
+
+import se.ichino.androidtestlibraryc.ClassForA;
+
+/**
+ * Created by mika on 2017. 2. 3..
+ */
+public class ClassA {
+  public static String getString() {
+    return ClassForA.callA();
+  }
+}
